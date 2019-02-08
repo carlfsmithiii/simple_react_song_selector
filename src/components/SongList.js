7257fa93e405aea09a3502e6c58a5c454939eb32
Carl Smith <cfsmithiii@gmail.com>
2019-02-08 11:52:27 -0500

    filled in mapStateToProps, mapDispatchToProps

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { selectSong } from "../actions";
 
 export class SongList extends Component {
   renderList() {
     return this.props.songs.map(song => (
       <div className="item" key={song.title}>
         <div className="right floated content">
-          <button className="ui button primary">Select</button>
+          <button
+            className="ui button primary"
+            onClick={() => this.props.selectSong(song)}
+          >
+            Select
+          </button>
         </div>
         <div className="content">{song.title}</div>
       </div>
@@ -18,11 +24,12 @@ export class SongList extends Component {
   }
 }
 
-const mapStateToProps = state => ({
-  songs: state.songs
-});
+const mapStateToProps = state => {
+  console.log(state);
+  return { songs: state.songs };
+};
 
-const mapDispatchToProps = {};
+const mapDispatchToProps = { selectSong };
 
 export default connect(
   mapStateToProps,
